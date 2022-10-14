69add70b9d5e79a69e3ae0da5bcb49e1512625b7
Manfred Touron <94029+moul@users.noreply.github.com>
2022-10-14 10:04:11 +0000

    chore: add basic example
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- demomod_test.go
+++ demomod_test.go
@@ -1,12 +1,23 @@
 package demomod_test
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	demomod "moul.io/adapterkit-module-demo"
 )
 
 func TestPackage(t *testing.T) {
-	var svc demomod.DemomodSvcServer = &demomod.Service{}
-	_ = svc
+	var _ demomod.DemomodSvcServer = &demomod.Service{}
+}
+
+func ExampleDemomodSvcSum_basic() {
+	svc := demomod.Service{}
+	ctx := context.TODO()
+	ret, err := svc.Sum(ctx, &demomod.Sum_Request{A: 42, B: 1000})
+	fmt.Println(ret, err)
+
+	// Output:
+	// c:1042 <nil>
 }
